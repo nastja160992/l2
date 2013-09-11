2994255d87a55bedf1406726b90cb694fe73d903
dragon-and-co <dragon-and-co@yandex.ru>
2013-09-11 18:37:41 +0300

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,21 +1,17 @@
-check_connection()
-{
-	return 1
-}
-
 TIME=$(awk -F "=" '/TIME/{print $2}' w.ini)
 
 while true
 do
-	if [ check_connection ];
+	if [ "$?" == "0" ];
 	then
-		echo "\n"
 		wget -q -O w.dump http://www.weathercity.com/by/minsk
 		sed 's/<[^<>]*>//g' w.dump | sed ':a;N;$!ba;s/\n/@/g' | \
 		grep -w -Po 'Current Minsk(.*)Forecast Summary' | sed 's/@ //g' | \
-		sed 's/@\+/@/g' | sed 's/&nbsp;/ /g' | sed 's/\s\+/ /g' | sed 's/@/\n/g' | cat
-		sleep $TIME
+		sed 's/@\+/@/g' | sed 's/&nbsp;/ /g' | sed 's/\s\+/ /g' | sed 's/@/\n/g' | \
+		grep 'Temperature:' | sed 's/Temperature://g' | cat
+		
 	else
 		echo "No Internet connection."
 	fi
+	sleep $TIME
 done
